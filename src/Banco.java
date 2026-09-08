ed237f1a894fe3afef8a8ec00f3acac743f389f2
NahueCampos <113687953+Nhuelcampos@users.noreply.github.com>
2026-09-08 19:15:02 -0300

    Update Banco.java

--- src/Banco.java
+++ src/Banco.java
@@ -1,15 +1,16 @@
 import java.util.Arrays;
 import java.util.Date;
-
+// Demostracion de la entidad bancaria asi coo listas de cajeros y cuenta bancaria 
 public class Banco {
+    //Atributos
     private Integer codigo;
     private String nombre;
     private String cuit;
     private String direccion;
     private Date fechaAlta;
-    private Cajero[] cajeros;
-    private CuentaBancaria[] cuentas;
-
+    private Cajero[] cajeros;                //Almacenar los  cajero asociados
+    private CuentaBancaria[] cuentas;        //Almacenar los  cuenta bancaria asociada 
+    // inicializa en la clase banco con sus datos principales y deja los arreglos de cajero y cuenta vacios
     public Banco(Integer codigo, String nombre, String cuit, String direccion, Date fechaAlta) {
         this.codigo = codigo;
         this.nombre = nombre;
@@ -19,7 +20,7 @@ public class Banco {
         this.cajeros = new Cajero[0];
         this.cuentas = new CuentaBancaria[0];
     }
-
+    // METODOS 
     public Integer getCodigo() {
         return codigo;
     }
@@ -47,26 +48,32 @@ public class Banco {
     public Date getFechaAlta() {
         return fechaAlta;
     }
+    // Gestion de cajeros 
+    //Agrega cajero si esta cumple con las condiciones establecidas 
 
     public void agregarCajero(Cajero cajero) {
         if (cajero == null) {
             return;
         }
+        //validar que el cajero sea del banco 
         if (cajero.getBanco() != this) {
             return;
         }
+        //Validar si ya existe un cajero registrado con el mismo número de serie dentro del banco 
         if (buscarCajero(cajero.getNumeroSerie()) != null) {
             return;
         }
-
+        //Crea un nuevo arreglo que copia todos los elementos del arreglo anterior pero le suma una posición adicional a su tamaño
         cajeros = Arrays.copyOf(cajeros, cajeros.length + 1);
+        //Asigna el nuevo cajero en la ultima posicion disponible de ese nuevo arreglo recien ampliado 
         cajeros[cajeros.length - 1] = cajero;
     }
+    //da copia del arreglo con los cajeros asociados al banco 
 
     public Cajero[] listarCajeros() {
         return Arrays.copyOf(cajeros, cajeros.length);
     }
-
+    // busca y devuelve cuenta bancaria apartir de su nro de cuenta y da null si no se encuentra 
     public Cajero buscarCajero(Integer numeroSerie) {
         for (Cajero cajero : cajeros) {
             if (cajero.getNumeroSerie().equals(numeroSerie)) {
@@ -75,6 +82,8 @@ public class Banco {
         }
         return null;
     }
+    // Cunetas bancarias 
+    //se agrega cuenta bancaria si no es nula y si no existe otra cuenta registrada con el mismo nro 
 
     public void agregarCuenta(CuentaBancaria cuenta) {
         if (cuenta == null) {
@@ -83,11 +92,12 @@ public class Banco {
         if (buscarCuenta(cuenta.getNumeroCuenta()) != null) {
             return;
         }
+        //Reorganiz ael arreglo y agrega la nueva
 
         cuentas = Arrays.copyOf(cuentas, cuentas.length + 1);
         cuentas[cuentas.length - 1] = cuenta;
     }
-
+    //busca cuenta bancaria y da null si no la encuentra 
     public CuentaBancaria buscarCuenta(Integer numeroCuenta) {
         for (CuentaBancaria cuenta : cuentas) {
             if (cuenta.getNumeroCuenta().equals(numeroCuenta)) {
